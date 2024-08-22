d980e5def09998d9b79f2d56cc21e1a17f463ed9
João Silva <joaogomesdev32@gmail.com>
2024-08-22 13:24:28 +0100

    curso de vue
    ate a aula 15

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>O meu nome é {{ name }} e trabalho como {{ profissao }}!</p>
+        <MultiplosEventos />
+    </div>
+</template>
+
+<script>
+    import MultiplosEventos from './MultiplosEventos.vue'
+
+    export default {
+        name: 'PrimeiroComponente',
+        components: {
+            MultiplosEventos
+        },
+        data() {
+            return {
+                name: 'João',
+                profissao: 'Programador'
+            }
+        }
+    }
+</script>
